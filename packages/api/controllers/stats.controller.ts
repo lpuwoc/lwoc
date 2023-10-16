1e32b8c9f333424cfca813bee6285a5d76c5eea2
Aryan Kumar <aryan.ak434@gmail.com>
2023-10-17 01:23:08 +0530

    add: github user stats route

---
+++ packages/api/controllers/stats.controller.ts
@@ -0,0 +1,31 @@
+import { Context } from "hono";
+import axios, { AxiosResponse } from "axios";
+
+export default class StatsController {
+    public stats = async (c: Context) => {
+        const username: string = c.req.param('username');
+
+        try {
+            var userData: AxiosResponse = await axios.get(`https://api.github.com/users/${username}`)
+            var userRepos: AxiosResponse = await axios.get(`https://api.github.com/users/${username}/repos`)
+        } catch (err: any) {
+            if (err.response.status == 404) {
+                return c.text('User not found', 404)
+            } else {
+                return c.text('Something went wrong', 500)
+            }
+        }
+
+        let repos: Array<Object> = []
+        userRepos.data.forEach((element: any) => {
+            let repo: object = {
+                id: element.id,
+                name: element.full_name
+            }
+            repos.push(repo)
+        });
+        userData.data.repos = repos
+
+        return c.json(userData.data)
+    }
+}
